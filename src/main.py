cc012012403b4a5f0d906d6aafc836773bfd0381
celcelcel <rickymjy@foxmail.com>
2025-10-16 16:29:04 +0800

    Revert "refactor: Update import statement for database model to improve clarity"
    This reverts commit 912970d9b83c4ae3ee2cb08dcb02d0e345aecef9.

--- src/main.py
+++ src/main.py
@@ -6,8 +6,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
-from src.models import db
-# from src.models.user import db
+from src.models.user import db
 from src.routes.user import user_bp
 from src.routes.note import note_bp
 from src.models.note import Note
